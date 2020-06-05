e0b3a70e59851110a73bd63794c13041d9bffbc0
Bojan <bojanhor@live.com>
2020-06-05 12:11:05 +0200

    Added GUI controller page template

---
+++ WebApplication1/GUI - Pages Custom/GuiController - EmptyTemplate.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System;
+
+namespace WebApplication1
+{
+    public partial class GuiController
+    {
+        public class EmptyTemplatePage : Dsps
+        {
+            public static string ViewStateElement_LucActiveOnGui = "LucActiveOnGui";
+
+            string Name;
+            Page thisPage;
+            System.Web.SessionState.HttpSessionState session;
+            public GControls.MasterMenuButton[] imagebuttons;
+            
+            public UpdatePanel UpdatePanel;
+            public AsyncPostBackTrigger Ap_UpdatePanel;
+            public Timer Tmr_UpdatePanel;
+                       
+                                               
+            public EmptyTemplatePage(Page _thisPage, System.Web.SessionState.HttpSessionState session)
+            {
+                this.session = session;
+                Name = PageHistory.GetPageNameFromPage(_thisPage);
+
+                try
+                {
+                    thisPage = _thisPage;                    
+                    ManageUpdatePanel();
+                  
+
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Internal error inside class constructor ( Pagename: "+ Name + "): " + ex.Message);
+                }
+            }
+
+            void ManageUpdatePanel()
+            {
+                try
+                {
+                    UpdatePanel = new UpdatePanel
+                    {
+                        UpdateMode = UpdatePanelUpdateMode.Conditional,
+                        ID = Name +"UpdatePanel"
+                    };
+
+                    Tmr_UpdatePanel = new Timer
+                    {
+                        Interval = Settings.UpdateValuesPCms,
+                        ID = Name + "Tmr_UpdatePanel"
+                    };
+
+                    Ap_UpdatePanel = new AsyncPostBackTrigger
+                    {
+                        ControlID = Name + "UpdatePanel"
+                    };
+
+                    UpdatePanel.Triggers.Add(Ap_UpdatePanel);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Error was encountered inside ManageUpdatePanel() method. page visited: "+ Name +". Error details: " + ex.Message);
+                }
+                
+            }
+                       
+            private void BtnMasterMenuClick(object sender, ImageClickEventArgs e, Page thisPage)
+            {
+                var me = (GControls.MasterMenuButton)sender;
+                Val.guiController.RedirectToPageOnButtonClick(thisPage, me.btnID);
+            }
+
+            private static int GethowManyButtonsOnFirstPage()
+            {
+                return XmlController.GetHowManyMenuItems();
+            }
+            
+
+            public void RegisterOnClick()
+            {
+
+            }
+                                  
+        }
+    }
+}
