ba909e5c430f28d3c19cba19e98faa5790c4dc1a
Bojan <bojanhor@live.com>
2021-07-13 08:36:38 +0200

    Installer added

---
+++ PrepareIIS/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.Administration;
+
+namespace PrepareIIS
+{
+    public partial class Form1 : Form
+    {
+        // Settings
+        public string Name = "testPool";
+
+        // //
+
+        ServerManager s = new ServerManager();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void AddSiteToIIS()
+        {
+            
+            for (int i = 0; i < s.ApplicationPools.Count; i++)
+            {
+                if (s.ApplicationPools[i].Name == Name)
+                {
+                    var pool = s.ApplicationPools[i];
+                    pool.Stop();
+                    ApplicationPoolCollection appColl = s.ApplicationPools;
+                    appColl.Remove(s.ApplicationPools[i]);
+                    
+
+                }
+            }
+
+            s.ApplicationPools.Add(Name);
+            
+        }
+    }
+}
